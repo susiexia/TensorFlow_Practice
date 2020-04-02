bbca11fe3d43379f844595f5b211825c447f3797
susiexia <57109686+susiexia@users.noreply.github.com>
2020-04-02 14:03:19 -0700

    restructure the folder of basic Neural Network

---
+++ Deep_Learning/deep_learning_IBM_HR.py
@@ -0,0 +1,14 @@
+# %% [markdown]
+
+# Dataset Resource: https://www.kaggle.com/pavansubhasht/ibm-hr-analytics-attrition-dataset
+
+# %% [markdown]
+# # imports and setup
+# %%
+import pandas as pd 
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+import tensorflow as tf 
+
+# %%
